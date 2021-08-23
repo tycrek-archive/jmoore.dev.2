0ae8c02b941be0a1a407cbb72f3a5fc4ac0aa1b0
tycrek <josh.moore@jmoore.dev>
2021-08-23 11:58:28 -0600

    tutorial redirects to old tutorial until I finish migrating it

--- routers/tutorials.js
+++ routers/tutorials.js
@@ -6,7 +6,6 @@ const router = require('express').Router();
 router.get('/', (_req, res) => res.render('tutorials', { isProd }));
 router.get('/:year/:month/:tutorial', (req, res, next) => {
 	const tutorial = `tutorials/${req.params.year}/${req.params.month}/${req.params.tutorial}/`;
-	console.log(path(tutorial));
 	fs.pathExists(path('views/pages', tutorial))
 		.then((exists) => !exists
 			? next()
